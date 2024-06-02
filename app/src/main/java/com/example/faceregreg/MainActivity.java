5a74bd6e1b99c4d46bb583c1412f109268a11eea
Flizzy50 <pattnaik.saril.2007@gmail.com>
2024-06-02 21:17:09 +0530

    Added buttons and assigned gallery button to open phone gallery when clicked

--- app/src/main/java/com/example/faceregreg/MainActivity.java
+++ app/src/main/java/com/example/faceregreg/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.faceregreg;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
     ImageView imageView;
     Button cameraBtn, galleryBtn;
+
+    //getting the image from gallery and displaying it
+    ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Uri imageUri = result.getData().getData();
+                    imageView.setImageURI(imageUri);
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
         galleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryActivityResultLauncher.launch(galleryIntent);
             }
         });
         cameraBtn.setOnClickListener(new View.OnClickListener() {
