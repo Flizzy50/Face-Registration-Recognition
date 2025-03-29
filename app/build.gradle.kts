da444c1252a6506429c044770d8b59f83910a052
Flizzy50 <pattnaik.saril.2007@gmail.com>
2025-03-29 22:53:17 +0530

    Upgraded android plugin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.tensorflow.lite.gpu)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
